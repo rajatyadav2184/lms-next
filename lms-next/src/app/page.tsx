17d3becdf1f1e4c0917077bc169c9e26da2fe921
Rajat <rajatyadav0804@gmail.com>
2024-02-10 14:48:21 +0530

    add description new for starter

--- lms-next/src/app/page.tsx
+++ lms-next/src/app/page.tsx
@@ -89,6 +89,7 @@ export default function Home() {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Explore starter templates for Next.js. for starter
+            new changes
           </p>
         </a>
 
